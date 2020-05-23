d3bc4a797a30d922ba0b0b2477ae649f014c0096
Gabriel Soares <gabriel.mello2016@gmail.com>
2020-05-22 21:56:14 -0300

    Atulizado inscrição de particioante, realizado ajustes na exibiçõa e implementado confirmação de inscrição

--- src/app/inscricao/inscricao/inscricao.page.ts
+++ src/app/inscricao/inscricao/inscricao.page.ts
@@ -1,9 +1,10 @@
-import { AlertsService } from './../../core/alerts.service';
-import { ErrorHandlerService } from './../../core/error-handler.service';
-import { InscricaoService, Evento } from './../inscricao.service';
+import { AlertController } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
 import { Router, Route, ActivatedRoute } from '@angular/router';
 import { NgForm } from '@angular/forms';
+import { AlertsService } from './../../core/alerts.service';
+import { ErrorHandlerService } from './../../core/error-handler.service';
+import { InscricaoService, Evento } from './../inscricao.service';
 
 @Component({
   selector: 'app-inscricao',
@@ -13,7 +14,7 @@ import { NgForm } from '@angular/forms';
 export class InscricaoPage implements OnInit {
 
   evento = new Evento();
-  codParticipante: number;
+  codParticipante = 8;
   atividades: any[] = [];
 
   constructor(
@@ -21,7 +22,8 @@ export class InscricaoPage implements OnInit {
     public handler: ErrorHandlerService,
     public alert: AlertsService,
     public router: Router,
-    public activeRoute: ActivatedRoute
+    public activeRoute: ActivatedRoute,
+    public alertController: AlertController
   ) { }
 
   ngOnInit() {
@@ -56,11 +58,33 @@ export class InscricaoPage implements OnInit {
       i.atividades = this.atividades;
     });
 
-    this.inscricaoService.cadastrar(inscricao)
-      .then(data => {
-        this.alert.alertaToast('Inscrição Finalizada', 'success');
-        this.router.navigate(['/home']);
-      })
-      .catch(erro => this.handler.handleError(erro));
+    this.confirmaInscricao(inscricao);
+  }
+
+  async confirmaInscricao(inscricao) {
+    const alert = await this.alertController.create({
+      cssClass: 'max-width',
+      header: 'Finalizar Inscrição',
+      message: 'Deseja realmente finalizar a inscrição nessas atividades?',
+      buttons: [
+        {
+          text: 'Não',
+          role: 'cancel'
+        }, {
+          text: 'Sim',
+          cssClass: 'success',
+          handler: (inscricao) => {
+            this.inscricaoService.cadastrar(inscricao)
+              .then(data => {
+                this.alert.alertaToast('Inscrição Finalizada', 'success');
+                this.router.navigate(['/home']);
+              })
+              .catch(erro => this.handler.handleError(erro));
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
 }
